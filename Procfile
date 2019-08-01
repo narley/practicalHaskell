9663ad18a7edc0204b7c7d304333bd40cf43fd15
James Bowen <jhbowen047@gmail.com>
2019-07-31 22:46:03 -0700

    Initial Heroku commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: run-final-server
---
+++ src/FinalServer.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+module FinalServer where
+
+import Data.Int (Int64)
+import qualified Data.Map as Map
+import Data.Proxy (Proxy(..))
+import Data.Text (Text)
+import Network.Wai.Handler.Warp (run)
+import Servant.API
+import Servant.Server
+import System.Environment (getEnv)
+
+type BasicAPI =
+  "api" :> "version" :> Get '[JSON] Int :<|>
+  "api" :> "name" :> Get '[JSON] Text :<|>
+  "api" :> "description" :> Get '[JSON] Text
+
+basicAPI :: Proxy BasicAPI
+basicAPI = Proxy :: Proxy BasicAPI
+
+versionHandler :: Handler Int
+versionHandler = return 2
+
+nameHandler :: Handler Text
+nameHandler = return "Basic Server"
+
+descriptionHandler :: Handler Text
+descriptionHandler = return "A basic server we constructed using Servant"
+
+basicServer :: Server BasicAPI
+basicServer =
+  versionHandler :<|>
+  nameHandler :<|>
+  descriptionHandler
+
+runServer :: IO ()
+runServer = do
+  portNum <- read <$> getEnv "PORT"
+  run portNum (serve basicAPI basicServer)
+
+nameDictionary :: Map.Map Int64 Text
+nameDictionary = Map.fromList
+  [ (1, "Karen")
+  , (2, "Anthony")
+  , (3, "Ashley")
+  , (4, "Julian")
+  , (5, "Christopher")
+  , (6, "Amanda")
+  ]
+
