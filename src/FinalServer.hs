f95379d42beb7cc4e687f702e07e10e9fd997d24
James Bowen <jhbowen047@gmail.com>
2019-08-02 19:56:45 -0700

    Implement final server

--- src/FinalServer.hs
+++ src/FinalServer.hs
@@ -4,50 +4,83 @@
 
 module FinalServer where
 
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Except (throwError)
+import Crypto.PasswordStore (verifyPassword, makePassword)
+import qualified Data.ByteString.Char8 as C
 import Data.Int (Int64)
 import qualified Data.Map as Map
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import Database.Esqueleto
+import Database.Persist.Postgresql (ConnectionString, Entity(..))
 import Network.Wai.Handler.Warp (run)
 import Servant.API
 import Servant.Server
-import System.Environment (getEnv)
+import System.Environment (lookupEnv)
+
+import Database (localConnString, runAction)
+import Schema
 
 type BasicAPI =
-  "api" :> "version" :> Get '[JSON] Int :<|>
-  "api" :> "name" :> Get '[JSON] Text :<|>
-  "api" :> "description" :> Get '[JSON] Text
+  "hello" :> Get '[JSON] Text :<|>
+  "create" :> ReqBody '[JSON] (User, Text) :> Post '[JSON] Int64 :<|>
+  "retrieve" :> BasicAuth "user" Int64 :> Capture "uid" Int64 :> Get '[JSON] User
 
 basicAPI :: Proxy BasicAPI
 basicAPI = Proxy :: Proxy BasicAPI
 
-versionHandler :: Handler Int
-versionHandler = return 2
+helloHandler :: Handler Text
+helloHandler = return "Hello Haskell Heroku"
 
-nameHandler :: Handler Text
-nameHandler = return "Basic Server"
+createHandler :: ConnectionString -> (User, Text) -> Handler Int64
+createHandler conn (user, password) = liftIO $ runAction conn $ do
+  userKey <- insert user
+  passwordHash <- liftIO $ makePassword (encodeUtf8 password) 17
+  insert (AuthData userKey passwordHash "user")
+  return (fromSqlKey userKey)
 
-descriptionHandler :: Handler Text
-descriptionHandler = return "A basic server we constructed using Servant"
+retrieveHandler :: ConnectionString -> Int64 -> Int64 -> Handler User
+retrieveHandler conn authId uid = if authId /= uid
+  then throwError $ err403 { errBody = "This is not the correct user!" }
+  else do
+    maybeUser <- liftIO $ runAction conn $ get (toSqlKey uid)
+    case maybeUser of
+      Nothing -> throwError $ err404 { errBody = "This user doesn't exist!" }
+      Just u -> return u
 
-basicServer :: Server BasicAPI
-basicServer =
-  versionHandler :<|>
-  nameHandler :<|>
-  descriptionHandler
+basicServer :: ConnectionString -> Server BasicAPI
+basicServer conn =
+  helloHandler :<|>
+  createHandler conn :<|>
+  retrieveHandler conn
+
+authCheck :: ConnectionString -> BasicAuthData -> IO (BasicAuthResult Int64)
+authCheck conn (BasicAuthData email password) = do
+  results <- runAction conn $ select . from $ \(users `InnerJoin` authData) -> do
+    on (users ^. UserId ==. authData ^. AuthDataUserId)
+    where_ (users ^. UserEmail ==. val (decodeUtf8 email))
+    return authData
+  if length results /= 1
+    then return NoSuchUser
+    else do
+      let (Entity _ authData) = head results
+      if verifyPassword password (authDataHashString authData)
+        then return $ Authorized (fromSqlKey (authDataUserId authData))
+        else return BadPassword
+
+authContext :: ConnectionString -> Context (BasicAuthCheck Int64 ': '[])
+authContext conn = BasicAuthCheck (authCheck conn) :. EmptyContext
 
 runServer :: IO ()
 runServer = do
-  portNum <- read <$> getEnv "PORT"
-  run portNum (serve basicAPI basicServer)
-
-nameDictionary :: Map.Map Int64 Text
-nameDictionary = Map.fromList
-  [ (1, "Karen")
-  , (2, "Anthony")
-  , (3, "Ashley")
-  , (4, "Julian")
-  , (5, "Christopher")
-  , (6, "Amanda")
-  ]
-
+  portString <- lookupEnv "PORT"
+  let portNum = case portString of
+        Nothing -> 8080
+        Just s -> read s
+  databaseString <- lookupEnv "DATABASE_URL"
+  let conn = case databaseString of
+        Nothing -> localConnString
+        Just s -> C.pack s
+  run portNum (serveWithContext basicAPI (authContext conn) (basicServer conn))
