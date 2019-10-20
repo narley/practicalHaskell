9b5386fc83c44da7b7939138462e0148d7c1997d
James Bowen <jhbowen047@gmail.com>
2019-10-20 12:33:31 -0700

    Fix tests

--- frontend/src/BasicPracticeTests.elm
+++ frontend/src/BasicPracticeTests.elm
@@ -17,14 +17,14 @@ main = Browser.sandbox {init = loadModel, update = update, view = view}
 loadModel : BPTestModel
 loadModel =
   { calculateAreaTests =
-    [ ("Circle 5 (0,0) should be 78.5398", 78.5398, calculateArea (Circle 5.0 {x: 0.0, y: 0.0})
-    , ("Circle 3 (3, 5) should be 28.2743", 28.27433, calculateArea (Circle 5.0 {x: 3.0, y: 5.0})
-    , ("Triangle (0,0) (2,0), (0, 2) should be 2", 2.0, calculateArea (Triangle {x: 0.0, y: 0.0} {x: 2.0, y: 0.0} {x: 0.0, y: 2.0})
-    , ("Triangle (5,5) (12,3), (8, 15) should be 38", 38.0, calculateArea (Triangle {x: 5.0, y: 5.0} {x: 12.0, y: 3.0} {x: 8.0, y: 15.0})
+    [ ("Circle 5 (0,0) should be 78.5398", 78.5398, calculateArea (Circle {radius=5.0, center= {x= 0.0, y= 0.0}}))
+    , ("Circle 3 (3, 5) should be 28.2743", 28.27433, calculateArea (Circle {radius=5.0, center= {x= 3.0, y= 5.0}}))
+    , ("Triangle (0,0) (2,0), (0, 2) should be 2", 2.0, calculateArea (Triangle {p1={x= 0.0, y= 0.0}, p2={x= 2.0, y= 0.0}, p3={x= 0.0, y= 2.0}}))
+    , ("Triangle (5,5) (12,3), (8, 15) should be 38", 38.0, calculateArea (Triangle {p1={x= 5.0, y= 5.0}, p2={x= 12.0, y= 3.0}, p3= {x= 8.0, y= 15.0}}))
     ]
   , sumsAndProductsTests =
     [ ("[] 3 5 should be []", [], sumsAndProducts [] 3 5)
-    , ("[1,2,3] 3 10 should be [6, 7, 5, 4, 4, 1]", [], sumsAndProducts [1, 2, 3] 3 10)
+    , ("[1,2,3] 3 10 should be [6, 7, 5, 4, 4, 1]", [6, 7, 5, 4, 4, 1], sumsAndProducts [1, 2, 3] 3 10)
     , ("[7, 3, 2, 9, 8] 2 9 should be [0, -5, 4, 3, 5, 5, -2, -9, -1, -7]", [0, -5, 4, 3, 5, 5, -2, -9, -1, -7], sumsAndProducts [7, 3, 2, 9, 8] 2 9)
     ]
   }
