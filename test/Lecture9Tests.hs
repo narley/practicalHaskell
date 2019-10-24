597fc31716a6a9d9ba5d02c4c446bf0ef77c7f18
James Bowen <jhbowen047@gmail.com>
2019-10-24 13:30:25 -0700

    Trim off extra Haskell code

--- test/Lecture9Tests.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Test.Tasty (defaultMain, testGroup, TestTree)
-import Test.Tasty.HUnit (testCase, (@?=))
-
-main :: IO ()
-main = defaultMain $ testGroup "Lecture 9 Tests"
-  []
