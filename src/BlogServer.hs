d5bffab83d9da250620d6975189b5b1a7d16519b
James Bowen <jhbowen047@gmail.com>
2019-10-23 18:50:28 -0700

    Blog server modifications

--- src/BlogServer.hs
+++ src/BlogServer.hs
@@ -5,24 +5,31 @@
 
 module BlogServer where
 
+import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class (liftIO)
+import Crypto.PasswordStore (verifyPassword)
+import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
 import Data.Int (Int64)
 import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
 import Database (runAction, localConnString)
 import Database.Persist (Entity(..), insert, get, update, delete, Update, (=.))
 import Database.Persist.Postgresql (ConnectionString, fromSqlKey, toSqlKey)
-import Database.Esqueleto (select, from)
+import Database.Esqueleto (select, from, orderBy, desc, limit, (^.), where_, (==.), val)
 import Network.HTTP.Media ((//), (/:))
 import Network.Wai (ResponseReceived, Response, mapResponseHeaders)
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.Servant.Options (provideOptions)
 import Servant.API
 import Servant.Server
 import Servant.Server.StaticFiles (serveDirectoryWebApp)
 
 
+import JWTHelpers (makeJWTCookie)
 import Schema
 
 type UsersCRUD = "api" :> "users" :>
@@ -30,7 +37,8 @@ type UsersCRUD = "api" :> "users" :>
     Capture "uid" Int64 :> Get '[JSON] (Maybe User) :<|>
     "create" :> ReqBody '[JSON] User :> Post '[JSON] Int64 :<|>
     "delete" :> Capture "uid" Int64 :> Delete '[JSON] () :<|>
-    "update" :> Capture "uid" Int64 :> ReqBody '[JSON] User :> Put '[JSON] ()
+    "update" :> Capture "uid" Int64 :> ReqBody '[JSON] User :> Put '[JSON] () :<|>
+    "login" :> ReqBody '[JSON] LoginInfo :> Post '[JSON] LoginResponse
   )
 
 getAllUsers :: ConnectionString -> Handler [Entity User]
@@ -55,13 +63,33 @@ updateUser conn uid user = liftIO $ runAction conn $ update (toSqlKey uid) userU
       , UserAge =. userAge user
       ]
 
+loginUser :: ConnectionString -> LoginInfo -> Handler LoginResponse
+loginUser conn (LoginInfo username password) = do
+  userEntityList <- liftIO $ runAction conn $ select . from $ \users -> do
+    where_ (users ^. UserEmail ==. val username)
+    return users
+  case userEntityList of
+    [(Entity uid _)] -> do
+      authDataE <- liftIO $ runAction conn $ select . from $ \authData -> do
+        where_ (authData ^. AuthDataUserId ==. val uid)
+        return authData
+      case authDataE of
+        [(Entity _ authData)] -> if verifyPassword (encodeUtf8 password) (authDataHashString authData)
+          then do
+            let cookie = makeJWTCookie (fromSqlKey uid)
+            return $ LoginResponse (fromSqlKey uid) cookie
+          else throwError $ err403 { errBody = "Invalid Password!"}
+        _ -> throwError $ err403 { errBody = "No Auth Data"}
+    _ -> throwError $ err403 { errBody = "Invalid user!"}
+
 usersServer :: ConnectionString -> Server UsersCRUD
 usersServer conn =
   getAllUsers conn :<|>
   retrieveUser conn :<|>
   createUser conn :<|>
   deleteUser conn :<|>
-  updateUser conn
+  updateUser conn :<|>
+  loginUser conn
 
 -- TODO Fill in these types, handler definitions, and add them to the Full API and Server at the bottom
 
@@ -70,7 +98,8 @@ type ArticlesCRUD = "api" :> "articles" :>
     Capture "uid" Int64 :> Get '[JSON] (Maybe Article) :<|>
     "create" :> ReqBody '[JSON] Article :> Post '[JSON] Int64 :<|>
     "delete" :> Capture "uid" Int64 :> Delete '[JSON] () :<|>
-    "update" :> Capture "uid" Int64 :> ReqBody '[JSON] Article :> Put '[JSON] ()
+    "update" :> Capture "uid" Int64 :> ReqBody '[JSON] Article :> Put '[JSON] () :<|>
+    "newest" :> QueryParam "limit" Int64 :> Get '[JSON] [Entity Article]
   )
 
 getAllArticles :: ConnectionString -> Handler [Entity Article]
@@ -96,13 +125,20 @@ updateArticle conn aid article = liftIO $ runAction conn $ update (toSqlKey aid)
       , ArticleAuthorId =. articleAuthorId article
       ]
 
+newestArticles :: ConnectionString -> Maybe Int64 -> Handler [Entity Article]
+newestArticles conn articleLimit = liftIO $ runAction conn $ select . from $ \articles -> do
+  orderBy [desc (articles ^. ArticlePublishedAt)]
+  limit (fromMaybe 3 articleLimit)
+  return articles
+
 articlesServer :: ConnectionString -> Server ArticlesCRUD
 articlesServer conn =
   getAllArticles conn :<|>
   retrieveArticle conn :<|>
   createArticle conn :<|>
   deleteArticle conn :<|>
-  updateArticle conn
+  updateArticle conn :<|>
+  newestArticles conn
 
 -- type CommentsCRUD = ...
 getAllComments :: ConnectionString -> Handler [Entity Comment]
@@ -173,11 +209,15 @@ addAllOriginsMiddleware :: Application -> Application
 addAllOriginsMiddleware baseApp req responseFunc = baseApp req newResponseFunc
   where
     newResponseFunc :: Response -> IO ResponseReceived
-    newResponseFunc = responseFunc . addOriginsAllowed
+    newResponseFunc = responseFunc . (addOriginsAllowed . addHeadersAllowed)
 
 addOriginsAllowed :: Response -> Response
 addOriginsAllowed = mapResponseHeaders $
   (:) ("Access-Control-Allow-Origin", "*")
 
+addHeadersAllowed :: Response -> Response
+addHeadersAllowed = mapResponseHeaders $
+  (:) ("Access-Control-Allow-Headers", "content-type")
+
 runServer :: IO ()
-runServer = run 8080 (addAllOriginsMiddleware (serve blogApi (blogServer localConnString)))
+runServer = run 8080 ((addAllOriginsMiddleware . (provideOptions fullCRUD)) (serve blogApi (blogServer localConnString)))
