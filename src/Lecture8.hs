1a43be6bdd34726d393e03150542c5d7bec5b2de
Narley Brittes <narley@brittes.co.uk>
2020-04-06 13:12:20 +0100

    lecture 8 answers

--- src/Lecture8.hs
+++ src/Lecture8.hs
@@ -8,20 +8,44 @@ import Database.Persist (Entity(..), Key)
 import Database.Persist.Postgresql (SqlPersistT)
 import Database.Esqueleto
 
+import qualified Data.Text as T
+import Data.List
+
 import Database
 import Schema
 
 lastYearsArticles :: SqlPersistT (LoggingT IO) [Entity Article]
-lastYearsArticles = undefined
+lastYearsArticles = select . from $ \articles -> do
+  where_ (articles ^. ArticlePublishedAt >. val (UTCTime (ModifiedJulianDay 58119) 0))
+  where_ (articles ^. ArticlePublishedAt <. val (UTCTime (ModifiedJulianDay 58483) 0))
+  orderBy [asc (articles ^. ArticleTitle)]
+  return articles
 
 getYoungUsers :: SqlPersistT (LoggingT IO) [Entity User]
-getYoungUsers = undefined
+getYoungUsers = select . from $ \users -> do
+  where_ (users ^. UserAge <. val 23)
+  orderBy [desc (users ^. UserName)]
+  limit 10
+  return users
 
 getSpecialPairs :: SqlPersistT (LoggingT IO) [(Entity User, Entity Article)]
-getSpecialPairs = undefined
+getSpecialPairs = select . from $ \(users `InnerJoin` articles) -> do
+  on (users ^. UserId ==. articles ^. ArticleAuthorId)
+  where_ ((users ^. UserName `like` val "E" ++. (%))
+      &&. (articles ^. ArticleTitle `like` val "E" ++. (%))
+      ||. (users ^. UserName `like` val "T" ++. (%))
+      &&. (articles ^. ArticleTitle `like` val "T" ++. (%)))
+  return (users, articles)
 
 getCommentsFromUser :: Key User -> SqlPersistT (LoggingT IO) [Entity Comment]
-getCommentsFromUser userId = undefined
+getCommentsFromUser userId = select . from $ \comments -> do
+  where_ (comments ^. CommentUserId ==. val userId)
+  orderBy [asc (comments ^. CommentArticleId), desc (comments ^. CommentSubmittedAt)]
+  return comments
 
 getCommentsOnUser :: Key User -> SqlPersistT (LoggingT IO) [Entity Comment]
-getCommentsOnUser userId = undefined
+getCommentsOnUser userId = select . from $ \(comments `InnerJoin` articles ) -> do
+  on $ articles ^. ArticleId ==. comments ^. CommentArticleId
+  where_ (articles ^. ArticleAuthorId ==. val userId)
+  orderBy [asc (comments ^. CommentSubmittedAt)]
+  return comments
