8877bcbbaf57401891d0b7db73460f3c5aad1fc0
Narley Brittes <narley@brittes.co.uk>
2020-04-06 13:12:10 +0100

    lecture 7 answers

--- src/Lecture7.hs
+++ src/Lecture7.hs
@@ -2,7 +2,6 @@ module Lecture7 where
 
 import Control.Monad
 import Control.Monad.Logger (LoggingT)
-import Data.List (sortBy)
 import Database.Persist (Entity(..), (<.), (>.), (==.), selectList, Filter(..), SelectOpt(..))
 import Database.Persist.Postgresql (SqlPersistT)
 
@@ -10,7 +9,14 @@ import Database (runAction, localConnString)
 import Schema
 
 getCommentsFromUser :: Key User -> SqlPersistT (LoggingT IO) [Entity Comment]
-getCommentsFromUser userId = undefined
+getCommentsFromUser uid =
+  selectList
+    [ CommentUserId ==. uid]
+    [ Asc CommentArticleId, Desc CommentSubmittedAt ]
 
 getCommentsOnUser :: Key User -> SqlPersistT (LoggingT IO) [Entity Comment]
-getCommentsOnUser userId = undefined
+getCommentsOnUser userId = do
+  articles <- selectList [ArticleAuthorId ==. userId] []
+  selectList [commentsOnArticles articles] [Asc CommentSubmittedAt]
+  where commentsOnArticles :: [Entity Article] -> Filter Comment
+        commentsOnArticles = FilterOr . fmap ((CommentArticleId ==.) . entityKey)
