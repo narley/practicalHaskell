4a1426494a9e2b66b0391ca6bc5e583b60cfc859
Narley Brittes <narley@brittes.co.uk>
2020-04-06 13:11:59 +0100

    lecture 6 answers

--- src/Lecture6.hs
+++ src/Lecture6.hs
@@ -3,7 +3,7 @@
 module Lecture6 where
 
 import Control.Monad (forM_)
-import Control.Monad.Logger (runStdoutLoggingT, LoggingT, LogLevel(..), filterLogger)
+import Control.Monad.Logger (runStdoutLoggingT, LoggingT, LogLevel(..), LogSource, filterLogger)
 import Control.Monad.Reader (runReaderT)
 import Data.List (sortBy)
 import Database.Persist (Entity(..), (<.), (>.), (==.), selectList, Filter(..), SelectOpt(..))
@@ -13,13 +13,17 @@ import qualified Data.Text as T
 import Database (localConnString)
 import Schema
 
+import Data.List
+
 runAction :: ConnectionString -> SqlPersistT (LoggingT IO) a -> IO a
-runAction conn action = runStdoutLoggingT $
+runAction conn action = runStdoutLoggingT . filterLogger logFilter $
   withPostgresqlConn conn (runReaderT action)
 
--- TODO: UNCOMMENT ME!
--- logFilter :: ???
--- logFilter = undefined
+logFilter :: LogSource -> LogLevel -> Bool
+logFilter _ LevelInfo = True
+logFilter _ LevelWarn = True
+logFilter _ LevelError = True
+logFilter _ _ = False
 
 fetchSpecialUsers :: IO ()
 fetchSpecialUsers = do
@@ -32,4 +36,18 @@ fetchSpecialUsers = do
       [Asc UserAge]
 
 fetchSpecialPairs :: IO [(Entity User, Entity Article)]
-fetchSpecialPairs = undefined
+fetchSpecialPairs = runAction localConnString $ do
+  allUsers <- usersQuery
+  allArticles <- articlesQuery
+  let allCombos = do
+        u <- allUsers
+        a <- filter (\a -> T.head (userName . entityVal $ u) == T.head (articleTitle . entityVal $ a)) allArticles
+        return (u, a)
+  return $ sortBy compareIds allCombos
+  where
+    usersQuery :: SqlPersistT (LoggingT IO) [Entity User]
+    usersQuery = selectList [] []
+    articlesQuery :: SqlPersistT (LoggingT IO) [Entity Article]
+    articlesQuery = selectList [] []
+    compareIds :: (Entity User, Entity Article) -> (Entity User, Entity Article) -> Ordering
+    compareIds ((Entity uid1 _), _) ((Entity uid2 _), _) = compare (fromSqlKey uid1) (fromSqlKey uid2)
