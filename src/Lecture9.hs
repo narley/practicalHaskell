ab4ff24191e80e3d6d9346e438f01ef44a78bd7d
Narley Brittes <narley@brittes.co.uk>
2020-04-07 19:34:02 +0100

    lecture 9 answers

--- src/Lecture9.hs
+++ src/Lecture9.hs
@@ -10,28 +10,58 @@ import Schema
 newtype UserWrapper = UserWrapper User
   deriving (Show, Eq)
 
+
+-- Exercise 9.c
 instance ToJSON UserWrapper where
-  toJSON = undefined
+  toJSON (UserWrapper user) = object
+    [ "userName" .= userName user
+    , "userEmail" .= userEmail user
+    , "userAge" .= userAge user
+    ]
 
 instance FromJSON UserWrapper where
-  parseJSON = undefined
+  parseJSON = withObject "UserWrapper" $ \o -> do
+    name <- o .: "userName"
+    email <- o .: "userEmail"
+    age <- o .: "userAge"
+    return $ UserWrapper (User name email age)
 
 
 newtype ArticleWrapper = ArticleWrapper Article
   deriving (Show, Eq)
 
 instance ToJSON ArticleWrapper where
-  toJSON = undefined
+  toJSON (ArticleWrapper article) = object
+    [ "articleTitle" .= articleTitle article
+    , "articleBody" .= articleBody article
+    , "articlePublishedAt" .= articlePublishedAt article
+    , "articleAuthorId" .= articleAuthorId article
+    ]
 
 instance FromJSON ArticleWrapper where
-  parseJSON = undefined
+  parseJSON = withObject "ArticleWrapper" $ \o -> do
+    title <- o .: "articleTitle"
+    body <- o .: "articleBody"
+    publishedAt <- o .: "articlePublishedAt"
+    authorId <- o .: "articleAuthorId"
+    return $ ArticleWrapper (Article title body publishedAt authorId)
 
 
 newtype CommentWrapper = CommentWrapper Comment
   deriving (Show, Eq)
 
 instance ToJSON CommentWrapper where
-  toJSON = undefined
+  toJSON (CommentWrapper comment) = object
+    [ "commentBody" .= commentBody comment
+    , "commentSubmittedAt" .= commentSubmittedAt comment
+    , "commentUserId" .= commentUserId comment
+    , "commentArticleId" .= commentArticleId comment
+    ]
 
 instance FromJSON CommentWrapper where
-  parseJSON = undefined
+  parseJSON = withObject "CommentWrapper" $ \o -> do
+    body <- o .: "commentBody"
+    submittedAt <- o .: "commentSubmittedAt"
+    userId <- o .: "commentUserId"
+    articleId <- o .: "commentArticleId"
+    return $ CommentWrapper (Comment body submittedAt userId articleId)
--- src/Schema.hs
+++ src/Schema.hs
@@ -41,4 +41,60 @@ PTH.share [PTH.mkPersist PTH.sqlSettings, PTH.mkMigrate "migrateAll"] [PTH.persi
     userId UserId
     articleId ArticleId
     deriving Show Read Eq
+
 |]
+
+-- Exercise 9.b
+deriveJSON (defaultOptions { fieldLabelModifier = dropXAndLowerFirst 4 }) ''User
+
+deriveJSON (defaultOptions { fieldLabelModifier = dropXAndLowerFirst 7 }) ''Article
+
+deriveJSON (defaultOptions { fieldLabelModifier = dropXAndLowerFirst 7 }) ''Comment
+
+
+-- Exercise 9.a
+-- instance ToJSON User where
+--   toJSON user = object
+--     [ "name" .= userName user
+--     , "email" .= userEmail user
+--     , "age" .= userAge user
+--     ]
+
+-- instance FromJSON User where
+--   parseJSON = withObject "User" $ \o -> do
+--     name' <- o .: "name"
+--     email' <- o .: "email"
+--     age' <- o .: "age"
+--     return $ User name' email' age'
+
+-- instance ToJSON Article where
+--   toJSON article = object
+--     [ "title" .= articleTitle article
+--     , "body" .= articleBody article
+--     , "publishedAt" .= articlePublishedAt article
+--     , "authorId" .= articleAuthorId article
+--     ]
+
+-- instance FromJSON Article where
+--   parseJSON = withObject "Article" $ \o -> do
+--     title' <- o .: "title"
+--     body' <- o .: "body"
+--     publishedAt' <- o .: "publishedAt"
+--     authorId' <- o .: "authorId"
+--     return $ Article title' body' publishedAt' authorId'
+
+-- instance ToJSON Comment where
+--   toJSON comment = object
+--     [ "body" .= commentBody comment
+--     , "submittedAt" .= commentSubmittedAt comment
+--     , "userId" .= commentUserId comment
+--     , "articleId" .= commentArticleId comment
+--     ]
+
+-- instance FromJSON Comment where
+--   parseJSON = withObject "Comment" $ \o -> do
+--     body' <- o .: "body"
+--     submittedAt' <- o .: "submittedAt"
+--     userId' <- o .: "userId"
+--     articleId' <- o .: "articleId"
+--     return $ Comment body' submittedAt' userId' articleId'
