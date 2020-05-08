5bf6d281a25f0d81e2b0543d28964347d4fcf587
Narley Brittes <narley@brittes.co.uk>
2020-05-08 17:11:57 +0100

    lecture-2 answers

--- frontend/src/BasicPractice.elm
+++ frontend/src/BasicPractice.elm
@@ -16,11 +16,27 @@ module BasicPractice exposing (..)
 
 
 -- TODO
-type alias Point = Float
-type Shape = Shape
+type alias Point =
+  { x: Float
+  , y: Float
+  }
+
+type Shape
+    = Circle {radius: Float, center: Point}
+    | Triangle {p1: Point, p2: Point, p3: Point}
 
 calculateArea : Shape -> Float
-calculateArea _ = 0.0
+calculateArea shape =
+    case shape of
+        Triangle {p1, p2, p3} ->
+            let
+                first = p1.x * (p2.y - p3.y)
+                second = p2.x * (p3.y - p1.y)
+                third = p3.x * (p1.y - p2.y)
+            in
+                abs ((first + second + third) / 2.0)
+        Circle {radius, center} -> pi * radius * radius
+
 
 -- sumsAndProducts :: [Int] -> Int -> Int -> [Int]
 -- sumsAndProducts is j k = do
@@ -34,7 +50,13 @@ calculateArea _ = 0.0
 -- sumsAndProducts [7, 3, 2, 9, 8] 2 9 = [0, -5, 4, 3, 5, 5, -2, -9, -1, -7]
 
 listAndThen : (a -> List a) -> List a -> List a
-listAndThen listFunc listElems = listElems
+listAndThen listFunc listElems =
+    case List.isEmpty listElems of
+        True -> []
+        False -> List.concatMap listFunc listElems
 
 sumsAndProducts : List Int -> Int -> Int -> List Int
-sumsAndProducts is j k = is
+sumsAndProducts is j k =
+    listAndThen (\i ->
+                     listAndThen (\l -> [k - l]) [i + j, i * j]
+                ) is
